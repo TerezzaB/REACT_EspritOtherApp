5c5e8a6d9299a1f59c5200d24de311e09cb8ac42
Terézia Boďová <bodova.terezia13@gmail.com>
2025-04-11 11:05:00 +0200

    2 - nav is gone rn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import ApiView from './views/ApiView'
-import HomeView from './views/HomeView'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -14,20 +13,18 @@ export default function App() {
     <>
       <Router>
         <div>
-          <div className="sticky-top p-2 _nav">
-            <div className="d-flex justify-content-center align-items-center space-x-4">
-              <NavLink to="/" className={({ isActive }) => `nav-link p-2 ${isActive ? 'text-primary font-weight-bold text-decoration-underline' : 'text-secondary'}`}>
-                Home
-              </NavLink>
-              <NavLink to="/api" className={({ isActive }) => `nav-link p-2 ${isActive ? 'text-primary font-weight-bold text-decoration-underline' : 'text-secondary'}`}>
-                API
-              </NavLink>
+          {/* 
+            <div className="sticky-top p-2 _nav">
+              <div className="d-flex justify-content-center align-items-center space-x-4">
+                <NavLink to="/" className={({ isActive }) => `nav-link p-2 ${isActive ? 'text-primary font-weight-bold text-decoration-underline' : 'text-secondary'}`}>
+                  API
+                </NavLink>
+              </div>
             </div>
-          </div>
+          */}
           <div className="container">
             <Routes>
-              <Route path="/" element={<HomeView />} />
-              <Route path="/api" element={<ApiView />} />
+              <Route path="/" element={<ApiView />} />
             </Routes>
           </div>
         </div>
