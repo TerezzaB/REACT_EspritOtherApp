2433db04b99b5876fc4d11ea23a597da7f434317
Terézia Boďová <bodova.terezia13@gmail.com>
2025-04-11 11:43:10 +0200

    5 - Time displayed

--- src/views/ApiView.jsx
+++ src/views/ApiView.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import DataTable from '../components/DataTable';
 import AutocompleteComponent from '../components/Autocomplete';
 
-// Utility function to format date as DD.MM.RRRR
+// Utility function to format date as HH:mm DD.MM.RRRR
 function formatDate(dateString) {
     const date = new Date(dateString);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
-    return `${day}.${month}.${year}`;
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes} ${day}.${month}.${year}`;
 }
 
 export default function ApiView() {
